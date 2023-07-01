49b5638f7759ba79baa0b5b829c87ee2c1820f37
eram-iw <eram.perween@iwebcode.dev>
2023-07-01 16:12:49 +0530

    trying to implement apis

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,7 +4,7 @@ import * as bcrypt from "bcrypt";
 interface RequestBody {
     name: string;
     email_phone: string;
-    password: string|null;
+    password: string | null;
     provider: string;
 }
 var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -12,23 +12,37 @@ var validPhoneRegex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/gm
 
 export async function POST(request: Request) {
     const body: RequestBody = await request.json();
-    if (validRegex.test(body.email_phone)  && body.provider===undefined) {
+    if (validRegex.test(body.email_phone) && body.provider === undefined) {
         body.provider = 'Email'
     } else if (validPhoneRegex.test(body.email_phone)) {
         body.provider = 'Phone'
     }
-    
-    const encryptedPassword = body.password !== null ?  await bcrypt.hash(body.password,10):null
-    const user = await prisma.users.create({
-        data: {
-            name: body.name,
-            email_phone: body.email_phone,
-            password: encryptedPassword as string,
-            provider: body.provider
-        },
+    var raw = JSON.stringify({
+        "email_phone": "eram@iwebcode.dev"
     });
 
-    const { password, ...result } = user;
-    return new Response(JSON.stringify(result), { status: 200 });
+    await fetch("http://localhost:3000/api/sendOtp", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: raw,
+        redirect: 'follow'
+    })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    const encryptedPassword = body.password !== null ? await bcrypt.hash(body.password, 10) : null
+    // const user = await prisma.users.create({
+    //     data: {
+    //         name: body.name,
+    //         email_phone: body.email_phone,
+    //         password: encryptedPassword as str77777777777777777ing,
+    //         provider: body.provider
+    //     },
+    // });
+
+    // const { password, ...result } = user;
+    // return new Response(JSON.stringify(result), { status: 200 });
     // return new Response(JSON.stringify("Incorrect Email or Phone Number"), { status: 401 });
 }
--- src/app/api/verifyOtp/route.ts
+++ src/app/api/verifyOtp/route.ts
@@ -1,19 +1,15 @@
-import { cookies } from 'next/headers';
 import { createHmac } from "crypto"
 const smsKey = process.env.SMS_SECRET_KEY as string;
 const jwt = require('jsonwebtoken');
 import { NextRequest, NextResponse } from 'next/server'
 
-
-const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN;
-const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN;
+const JWT_AUTH_TOKEN = process.env.JWT_SECRET;
 
 interface RequestBody {
     email_phone: string;
     hash: string;
     otp: number;
 }
-let refreshTokens = [];
 
 export async function POST(request: NextRequest) {
     const body: RequestBody = await request.json();
@@ -31,9 +27,6 @@ export async function POST(request: NextRequest) {
     if (newCalculatedHash === hashValue) {
         console.log('user confirmed');
         const accessToken = jwt.sign({ data: email_phone }, JWT_AUTH_TOKEN, { expiresIn: '30s' });
-        const refreshToken = jwt.sign({ data: email_phone }, JWT_REFRESH_TOKEN, { expiresIn: '1y' });
-        // const response = NextResponse.next()
-        refreshTokens.push(refreshToken);
         const response = NextResponse.json(
             { msg: 'Device verified' },
             { status: 200 }
@@ -45,26 +38,11 @@ export async function POST(request: NextRequest) {
             sameSite: 'strict',
             expires: new Date(new Date().getTime() + 30 * 1000),
         },
-        )
-        response.cookies.set({
-            name: 'refreshToken',
-            value: refreshToken,
-            expires: new Date(new Date().getTime() + 31557600000),
-            sameSite: 'strict',
-            httpOnly: true
-        },
-        )
+        )        
         response.cookies.set({
             name: 'authSession',
             value: 'true',
             expires: new Date(new Date().getTime() + 30 * 1000),
-            sameSite: 'strict'
-        },
-        )
-        response.cookies.set({
-            name: 'refreshTokenID',
-            value: 'true',
-            expires: new Date(new Date().getTime() + 31557600000),
             sameSite: 'strict'
         },
         )
